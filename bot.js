bb756c0a9e61b224f5cfb89d3c1564c6fb7e4cba
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-06-08 15:11:20 +0100

    telegram bot

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "7474472686:AAGgbvyGnnuigKaeHkTE290RXSCBWVXlZP0"; // Replace with your own bot token
+const token = process.env.TOKEN; // Replace with your own bot token
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", (msg) => {
