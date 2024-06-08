e6cd5e2fb2ba0fb7806acec24ecc0ab33996451d
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-06-08 15:38:43 +0100

    added axios and express

--- bot.js
+++ bot.js
@@ -1,5 +1,19 @@
 const TelegramBot = require("node-telegram-bot-api");
 
+const axios = require("axios");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
+
 const token = process.env.TOKEN; // Replace with your own bot token
 const bot = new TelegramBot(token, { polling: true });
 
